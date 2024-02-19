71e396fcaa377c14b8d784082cb6d73a4e4771ed
Riley DuBord <thelifeofdu@gmail.com>
2024-02-19 17:59:19 -0600

    backend for social tab semi complete

--- server/routes/friend.router.js
+++ server/routes/friend.router.js
@@ -19,11 +19,37 @@ router.get("/", (req, res) => {
       });
   });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
-});
+  router.post("/", (req, res) => {
+    console.log(req.body);
+    const insertFriendQuery = `
+  INSERT INTO "friends"
+  ("user_id", "friend")
+  VALUES
+  ($1, $2)
+  `;
+    const insertFriendValues = [req.body];
+    pool
+      .query(insertFriendQuery, [insertFriendValues])
+      //   above query param needs a check
+      .then((result) => {
+        res.send(result.rows);
+      })
+      .catch((err) => {
+        console.error("ERROR: POSTing a friendship", err);
+        res.sendStatus(500);
+      });
+  });
+
+router.delete("/:id", (req, res) => {
+    pool
+      .query('DELETE FROM "friends" WHERE user_id=$1 AND friend=$2', [req.user.id, req.params.id])
+      .then((result) => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.error("Error in DELETE a friend", err);
+        res.sendStatus(500);
+      });
+  });
 
 module.exports = router;
