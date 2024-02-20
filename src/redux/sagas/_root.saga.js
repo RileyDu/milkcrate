c3107395173dda4765fdd4264a94757b5dbc52c1
Riley DuBord <thelifeofdu@gmail.com>
2024-02-20 16:23:41 -0600

    social saga established, still needs search feature

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -3,6 +3,7 @@ import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
 import spinsSaga from './spins.saga';
+import socialSaga from './social.saga';
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
 // This is imported in index.js as rootSaga
@@ -16,5 +17,6 @@ export default function* rootSaga() {
     registrationSaga(),
     userSaga(),
     spinsSaga(),
+    socialSaga(),
   ]);
 }
---
+++ src/redux/sagas/social.saga.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { put, takeLatest } from "redux-saga/effects";
+
+// worker Saga: will be fired on "FETCH_USER" actions
+function* fetchFriends() {
+  try {
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+    const response = yield axios.get("/api/social/friends", config);
+    yield put({ type: "SET_FRIENDS", payload: response.data });
+  } catch (error) {
+    console.error("User get request failed", error);
+  }
+}
+
+function* postFriendship() {
+  try {
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+    const response = yield axios.post("/api/social/add", config);
+    yield put({ type: "FETCH_FRIENDS" });
+  } catch (error) {
+    console.error("User get request failed", error);
+  }
+}
+
+function* deleteFriendship() {
+    try {
+      const config = {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      };
+      const response = yield axios.delete("/api/social/delete", config);
+      yield put({ type: "FETCH_FRIENDS" });
+    } catch (error) {
+      console.error("User get request failed", error);
+    }
+  }
+
+  function* fetchFriends() {
+    try {
+      const config = {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      };
+      const response = yield axios.get("/api/social/friends/collection", config);
+      yield put({ type: "SET_RECORDS", payload: response.data });
+    } catch (error) {
+      console.error("User get request failed", error);
+    }
+  }
+
+
+//   NEED TO IMPLEMENT SEARCH STILL
+
+function* socialSaga() {
+  yield takeLatest("FETCH_FRIENDS", fetchFriends);
+  yield takeLatest("POST_FRIENDSHIP", postFriendship);
+  yield takeLatest("DELETE_FRIENDSHIP", deleteFriendship);
+  yield takeLatest("FETCH_FRIENDS_RECORDS", fetchFriendsRecords);
+
+}
+
+export default socialSaga;
