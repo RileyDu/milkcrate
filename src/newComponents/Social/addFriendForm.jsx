91e26a6eb40b6f9ba8c3c40cb4d808624f09687b
Riley DuBord <thelifeofdu@gmail.com>
2024-02-20 14:09:30 -0600

    skeleton for addFriendForm started, need to branch

--- src/newComponents/Social/addFriendForm.jsx
+++ src/newComponents/Social/addFriendForm.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import {useSelector} from 'react-redux';
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
-function AddFriendForm(props) {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
-//   const store = useSelector((store) => store);
-//   const [heading, setHeading] = useState('Functional Component');
 
+function AddFriendForm(props) {
+const [friendUsername, setFriendUsername] = useState('')
+
+const handleInputChangeFriend = (e) => {
+  setFriendUsername(e.target.value)
+}
   return (
     <div>
       <h2>In addFriendForm</h2>
+      <input
+              type="text"
+              value={friendUsername}
+              onChange={handleInputChangeFriend}
+            />
     </div>
   );
 }
