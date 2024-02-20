1870d3e5a9bb89382f6263c240faaa966dc429ab
Riley DuBord <thelifeofdu@gmail.com>
2024-02-20 14:18:23 -0600

    addFriendForm finished, yet to test

--- src/newComponents/Social/addFriendForm.jsx
+++ src/newComponents/Social/addFriendForm.jsx
@@ -1,21 +1,42 @@
-import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
-
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 function AddFriendForm(props) {
-const [friendUsername, setFriendUsername] = useState('')
+  const dispatch = useDispatch()
+  const [friendUsername, setFriendUsername] = useState("");
 
-const handleInputChangeFriend = (e) => {
-  setFriendUsername(e.target.value)
+  const handleInputChangeFriend = (e) => {
+    setFriendUsername(e.target.value);
+  };
+
+function postFriendship(event) {
+  event.preventDefault()
+  console.log('checking payload of submit', friendUsername);
+  
+  if (!friendUsername) {
+    alert('PLEASE FILL FORM BEFORE SUBMIT')
+    return;
+  }
+
+  dispatch({
+    type: "POST_FRIENDSHIP",
+    payload: friendUsername
+  })
+  setFriendUsername('');
 }
+
   return (
     <div>
       <h2>In addFriendForm</h2>
+      <form onSubmit={(event) => postFriendship(event)}>
       <input
-              type="text"
-              value={friendUsername}
-              onChange={handleInputChangeFriend}
-            />
+        type="text"
+        value={friendUsername}
+        onChange={handleInputChangeFriend}
+      />
+      <button type='submit'>ADD FRIEND</button>
+</form>
     </div>
   );
 }
