9fd6181fc137f034e5a0158798f7c4e95d360da6
Riley DuBord <thelifeofdu@gmail.com>
2024-02-20 16:17:21 -0600

    blindbag saga established, yet to test

---
+++ src/redux/sagas/blindbag.saga.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+// worker Saga: will be fired on "FETCH_USER" actions
+function* fetchBlindbag() {
+  try {
+    const config = {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    };
+    const response = yield axios.get('/api/blindbag', config);
+    yield put({ type: 'SET_BLINDBAG', payload: response.data });
+  } catch (error) {
+    console.error('User get request failed', error);
+  }
+}
+
+function* blindbagSaga() {
+  yield takeLatest('FETCH_BLINDBAG', fetchBlindbag);
+}
+
+export default blindbagSaga;
