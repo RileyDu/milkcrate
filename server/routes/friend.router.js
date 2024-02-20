b8deb9fe8c5a69ba48ce92ae5099be81cce5a58a
Riley DuBord <thelifeofdu@gmail.com>
2024-02-20 09:50:12 -0600

    friendship GET backend now displays both usernames

--- server/routes/friend.router.js
+++ server/routes/friend.router.js
@@ -1,49 +1,57 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 const {
-    rejectUnauthenticated,
-  } = require('../modules/authentication-middleware');
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
 
 // GET all of a user's friends
-router.get("/", rejectUnauthenticated, (req, res) => {
-    const query = `
-      SELECT * FROM "friends"
-      WHERE user_id = $1
-        ORDER BY "id" DESC;
+router.get("/friends", rejectUnauthenticated, (req, res) => {
+  const query = `
+    SELECT 
+    friends.id AS friendship_id, 
+    friends.user_id AS user_id, 
+    "user".username AS user_username,
+    friends.friend AS friend_user_id,  
+    friend_user.username AS friend_username
+FROM "friends"
+INNER JOIN "user" ON friends.user_id = "user".id
+INNER JOIN "user" AS friend_user ON friends.friend = friend_user.id
+WHERE friends.user_id = 1
+ORDER BY friends.id DESC;
     `;
-    pool
-      .query(query, [req.user.id])
-      .then((result) => {
-        res.send(result.rows);
-      })
-      .catch((err) => {
-        console.error("ERROR: Get a user's friends", err);
-        res.sendStatus(500);
-      });
-  });
+  pool
+    .query(query, [req.user.id])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.error("ERROR: Get a user's friends", err);
+      res.sendStatus(500);
+    });
+});
 
 // GET a friend's collection
-  router.get("/", rejectUnauthenticated, (req, res) => {
-    const query = `
+router.get("/friends/collection", rejectUnauthenticated, (req, res) => {
+  const query = `
       SELECT * FROM "albums"
       WHERE user_id = $1
         ORDER BY "id" DESC;
     `;
-    pool
-      .query(query, [req.body.id])
-      .then((result) => {
-        res.send(result.rows);
-      })
-      .catch((err) => {
-        console.error("ERROR: Get a user's albums", err);
-        res.sendStatus(500);
-      });
-  });
+  pool
+    .query(query, [req.body.id])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.error("ERROR: Get a user's albums", err);
+      res.sendStatus(500);
+    });
+});
 
-  //GET albums from friend's milkcrate W/SEARCH params
-router.get("/", rejectUnauthenticated,  (req, res) => {
-    const query = `
+//GET albums from friend's milkcrate W/SEARCH params
+router.get("/friends/collection/search", rejectUnauthenticated, (req, res) => {
+  const query = `
     SELECT *
     FROM "albums"
     WHERE
@@ -52,51 +60,53 @@ router.get("/", rejectUnauthenticated,  (req, res) => {
         OR "tags" % $1
         OR CAST("mood" AS TEXT) % $1;
     `;
-    pool
-      .query(query, [req.params])
-      .then((result) => {
-        res.send(result.rows);
-      })
-      .catch((err) => {
-        console.error("ERROR: seaching user's milkcrate", err);
-        res.sendStatus(500);
-      });
-  });
+  pool
+    .query(query, [req.params])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.error("ERROR: seaching user's milkcrate", err);
+      res.sendStatus(500);
+    });
+});
 
-  // POST a new frienship
-  router.post("/", rejectUnauthenticated, (req, res) => {
-    console.log(req.body);
-    const insertFriendQuery = `
+// POST a new frienship
+router.post("/add", rejectUnauthenticated, (req, res) => {
+  console.log(req.body);
+  const insertFriendQuery = `
   INSERT INTO "friends"
   ("user_id", "friend")
   VALUES
   ($1, $2)
   `;
-    const insertFriendValues = [req.body];
-    pool
-      .query(insertFriendQuery, [insertFriendValues])
-      //   above query param needs a check
-      .then((result) => {
-        res.send(result.rows);
-      })
-      .catch((err) => {
-        console.error("ERROR: POSTing a friendship", err);
-        res.sendStatus(500);
-      });
-  });
-
+  const insertFriendValues = [req.body];
+  pool
+    .query(insertFriendQuery, [insertFriendValues])
+    //   above query param needs a check
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.error("ERROR: POSTing a friendship", err);
+      res.sendStatus(500);
+    });
+});
 
 // DELETE a friendship
 router.delete("/:id", (req, res) => {
-    pool
-      .query('DELETE FROM "friends" WHERE user_id=$1 AND friend=$2', [req.user.id, req.params.id])
-      .then((result) => {
-        res.sendStatus(200);
-      })
-      .catch((err) => {
-        console.error("Error in DELETE a friend", err);
-        res.sendStatus(500);
-      });
-  });
+  pool
+    .query('DELETE FROM "friends" WHERE user_id=$1 AND friend=$2', [
+      req.user.id,
+      req.params.id,
+    ])
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error("Error in DELETE a friend", err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
