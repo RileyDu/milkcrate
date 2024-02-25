f64e2b2d49c3f3e709d345a90e20149b11acd409
Riley DuBord <thelifeofdu@gmail.com>
2024-02-25 16:02:45 -0600

    styled blindbag page

--- src/newComponents/BlindBag/blindbag.jsx
+++ src/newComponents/BlindBag/blindbag.jsx
@@ -1,22 +1,29 @@
-import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 function Blindbag(props) {
   const dispatch = useDispatch();
-  const randomRecord = useSelector((store) => store.blindbagReducer)
+  const randomRecord = useSelector((store) => store.blindbagReducer);
 
-  function getBlindBag(){
+  function getBlindBag() {
     dispatch({ type: "FETCH_BLINDBAG" });
   }
 
   return (
     <div>
-      <h2 className='header-tabs'>blindbag</h2>
-      <button onClick={()=>getBlindBag()}>SHUFFLE</button>
-      <img src={randomRecord.coverart} alt={randomRecord.title} />
+      <h2 className="header-tabs">blindbag</h2>
+      {/* <img src="milkcrateLogo.svg" className="blingbag-crate"/> */}
+      <div className="container">
+      <div className="d-grid gap-2">
+      <button className='btn btn-lg btn-primary' onClick={() => getBlindBag()}>SHUFFLE</button>
     </div>
+    </div>
+    <div className="blindbag-img">
+      <img src={randomRecord.coverart} alt={randomRecord.title} />
+  </div>
+  </div>
   );
 }
 
