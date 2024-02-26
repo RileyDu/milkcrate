bc69e96c4436fa40c0f3b439b1c787c949dad597
Riley DuBord <thelifeofdu@gmail.com>
2024-02-26 13:33:12 -0600

    spin details page can now refresh w/o crashing

--- src/newComponents/SpinSessions/singleSpin.jsx
+++ src/newComponents/SpinSessions/singleSpin.jsx
@@ -3,10 +3,9 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom";
 import dayjs from "dayjs";
-import Swal from 'sweetalert2'
-
+import Swal from "sweetalert2";
 
 function SingleSpin(props) {
   const { id } = useParams();
@@ -19,18 +18,18 @@ function SingleSpin(props) {
 
   useEffect(() => {
     dispatch({ type: "FETCH_SINGLE_SPIN", payload: id });
-  }, [dispatch]);
+  }, [dispatch, id]);
 
   function deleteSpin() {
     // Show SweetAlert confirmation dialog
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       text: "This will permanently delete the spin. You won't be able to revert this!",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
         // User confirmed deletion, proceed with the delete operation
@@ -39,43 +38,43 @@ function SingleSpin(props) {
           payload: id,
         });
         history.push(`/spins`); // Redirects user back to the spins page
-  
+
         // Show deletion success message
-        Swal.fire(
-          'Deleted!',
-          'Your spin has been deleted.',
-          'success'
-        );
+        Swal.fire("Deleted!", "Your spin has been deleted.", "success");
       }
       // If result.isConfirmed is false, user canceled, do nothing
     });
   }
 
-  const formattedDate = dayjs(spins[0].listened_at).format('MM/DD/YYYY')
-  const takeOffSeconds = spins[0].time_spent.split(":")
-  const formattedTime = takeOffSeconds[0] + ":" + takeOffSeconds[1]
+  const spinExists = spins && spins.length > 0;
+  const formattedDate = spinExists ? dayjs(spins[0].listened_at).format('MM/DD/YYYY') : '';
+  const takeOffSeconds = spinExists ? spins[0].time_spent.split(":") : ['00', '00'];
+  const formattedTime = `${takeOffSeconds[0]}:${takeOffSeconds[1]}`;
 
   return (
     <div>
       <h2 className="header-tabs">spin details</h2>
       <button onClick={() => deleteSpin()}>delete spin</button>
-      <p>listened on {formattedDate} </p>
-      <p>{formattedTime} spent listening</p>
-      <p><strong> DETAILS</strong></p>
-      <p>{spins[0].spin_details}</p>
-      <p><strong> albums listened to</strong></p>
-      <ul>
-        {spins.map((spin, i) => (
-          <li key={i}>
-            <p>
-              {spin.title} by {spin.artist}
-            </p>
-          </li>
-        ))}
-      </ul>
+      {spinExists ? (
+        <>
+          <p>listened on {formattedDate}</p>
+          <p>{formattedTime} spent listening</p>
+          <p><strong> DETAILS</strong></p>
+          <p>{spins[0].spin_details}</p>
+          <p><strong> albums listened to</strong></p>
+          <ul>
+            {spins.map((spin, i) => (
+              <li key={i}>
+                <p>{spin.title} by {spin.artist}</p>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>Loading spin details...</p>
+      )}
     </div>
     // NEED TO ADD CONDITIONAL RENDERING DEPENDING ON HOW MANY ALBUMS IN A LISTENING SESSION STILL
-    //NEED TO USE MOMENT TO TRANSLATE DB TIME TO READABLE FORM
   );
 }
 
